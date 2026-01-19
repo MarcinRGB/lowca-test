cac35b5810696ea5efa9bd949b7dc49cf03da7c0
MarcinRGB <marcin92tomczyk@gmail.com>
2026-01-19 05:23:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
-print("Python działa")
+import os
+from telegram import Bot
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+bot = Bot(token=TOKEN)
+bot.send_message(chat_id=CHAT_ID, text="✅ Bot działa!")
